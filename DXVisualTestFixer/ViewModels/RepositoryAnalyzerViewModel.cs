cef4fb645edc25eabb5020fa226b25467b2c61f3
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-04-18 14:48:50 +0300

    sync

--- DXVisualTestFixer/ViewModels/RepositoryAnalyzerViewModel.cs
+++ DXVisualTestFixer/ViewModels/RepositoryAnalyzerViewModel.cs
@@ -10,12 +10,34 @@ namespace DXVisualTestFixer.ViewModels {
     public interface IRepositoryAnalyzerViewModel : ISupportParameter { }
 
     public class TimingModel {
-        public TimingModel(string name, TimeSpan time) {
-            Name = name;
+        public TimingModel(string fullName, TimeSpan time) {
+            FullName = fullName;
             Time = time;
+            PopulateAttributes();
         }
 
-        public string Name { get; }
+        void PopulateAttributes() {
+            string[] splitted = FullName.Split(new string[] { "_", "-" }, StringSplitOptions.RemoveEmptyEntries);
+            if(splitted.Length < 1)
+                return;
+            Prefix = splitted[0];
+            if(splitted.Length < 2)
+                return;
+            Team = splitted[1];
+            if(splitted.Length < 3)
+                return;
+            Dpi = splitted[2];
+            if(splitted.Length < 4)
+                return;
+            Part = splitted[3];
+        }
+
+        public string FullName { get; }
+        public string Prefix { get; private set; }
+        public string Team { get; private set; }
+        public string Dpi { get; private set; }
+        public string Part { get; private set; }
+
         public TimeSpan Time { get; }
     }
 
