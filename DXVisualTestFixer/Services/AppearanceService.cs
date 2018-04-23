313cff03fd27ec2a7fcd7c0fb9453286fd6a7afa
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-04-23 12:24:50 +0300

    continue

--- DXVisualTestFixer/Services/AppearanceService.cs
+++ DXVisualTestFixer/Services/AppearanceService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DXVisualTestFixer {
+namespace DXVisualTestFixer.Services {
     public interface IAppearanceService {
         void SetTheme(string themeName);
     }
