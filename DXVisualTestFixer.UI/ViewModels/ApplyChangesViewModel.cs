9202fd5dc28638b38872e3b4a96db19d64848d9d
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-05-10 16:45:26 +0300

    continue

--- DXVisualTestFixer.UI/ViewModels/ApplyChangesViewModel.cs
+++ DXVisualTestFixer.UI/ViewModels/ApplyChangesViewModel.cs
@@ -3,17 +3,24 @@ using DevExpress.Xpf.Core;
 using DXVisualTestFixer.Common;
 using Microsoft.Practices.Unity;
 using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BindableBase = Prism.Mvvm.BindableBase;
 
 namespace DXVisualTestFixer.UI.ViewModels {
     public class ApplyChangesViewModel : BindableBase, IApplyChangesViewModel {
+        List<ITestInfoWrapper> _ChangedTests;
+
         public IEnumerable<UICommand> DialogCommands { get; private set; }
 
-        public List<ITestInfoWrapper> ChangedTests { get; }
+        public List<ITestInfoWrapper> ChangedTests {
+            get { return _ChangedTests; }
+            private set { SetProperty(ref _ChangedTests, value); }
+        }
 
         public bool Confirmed { get; set; }
         public string Title { get;  set; }
@@ -21,11 +28,11 @@ namespace DXVisualTestFixer.UI.ViewModels {
 
         public IEnumerable<UICommand> Commands { get; }
 
-        public ApplyChangesViewModel(IUnityContainer container) {
+        public ApplyChangesViewModel(IMainViewModel mainViewModel) {
             Title = "Settings";
             Commands = UICommand.GenerateFromMessageButton(MessageButton.OKCancel, new DialogService(), MessageResult.OK, MessageResult.Cancel);
             Commands.Where(c => c.IsDefault).Single().Command = new DelegateCommand(() => Confirmed = true);
-            ChangedTests = container.Resolve<IMainViewModel>().GetChangedTests();
+            ChangedTests = mainViewModel.GetChangedTests();
         }
     }
 }
--- DXVisualTestFixer/Config/VersionInfo.cs
+++ DXVisualTestFixer/Config/VersionInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DXVisualTestFixer.Configuration {
     public static class VersionInfo {
-        public const string VersionString = "2.0.19";
+        public const string VersionString = "2.1.0";
         public static readonly Version Version = new Version(VersionString);
     }
 }
