af69521f251eeff61ba68e0ceaf2faa3925352c0
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-06-13 12:12:07 +0300

    add 18.2

--- DXVisualTestFixer.Common/Repository.cs
+++ DXVisualTestFixer.Common/Repository.cs
@@ -2,7 +2,7 @@
 
 namespace DXVisualTestFixer.Common {
     public class Repository {
-        public static readonly string[] Versions = new string[] { "16.1", "16.2", "17.1", "17.2", "18.1" };
+        public static readonly string[] Versions = new string[] { "16.1", "16.2", "17.1", "17.2", "18.1", "18.2" };
 
         public string Version { get; set; }
         public string Path { get; set; }
