7df8352dd32c7cabf7080ba0af75ab38f35fc5ed
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-10-11 18:32:14 +0300

    open solution improvements

--- DXVisualTestFixer.UI/Models/SolutionModel.cs
+++ DXVisualTestFixer.UI/Models/SolutionModel.cs
@@ -1,27 +1,60 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using DevExpress.Utils;
 using DXVisualTestFixer.Common;
+using Microsoft.Win32;
 
 namespace DXVisualTestFixer.UI.Models {
-    public class SolutionModel {
+    public class SolutionModel : ImmutableObject {
+        readonly string SolutionPath;
+
         public SolutionModel(string version, string path) {
             Version = version;
             Path = GetRealPath(path);
+            SolutionPath = Directory.EnumerateFiles(Path, "*.sln", SearchOption.TopDirectoryOnly).FirstOrDefault();
+            Image = GetImage();
         }
 
         public string Version { get; }
         public string Path { get; }
+        public ImageSource Image { get; }
+
+        ImageSource GetImage() {
+            if(SolutionPath == null || !File.Exists(SolutionPath))
+                return null;
+            Icon appIcon = Icon.ExtractAssociatedIcon(SolutionPath);
+            Bitmap bitmap = appIcon.ToBitmap();
+            IntPtr hBitmap = bitmap.GetHbitmap();
+
+            return Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
         public void OpenSolution() {
-            var solutionFilePath = Directory.EnumerateFiles(Path, "*.sln", SearchOption.TopDirectoryOnly).FirstOrDefault();
-            if(solutionFilePath == null || !File.Exists(solutionFilePath))
+            if(SolutionPath == null || !File.Exists(SolutionPath))
                 return;
-            string openSolutionPath = @"C:\Program Files (x86)\Common Files\Microsoft Shared\MSEnv\VSLauncher.exe";
-            if(!File.Exists(openSolutionPath))
+            string associatedProgramPath = GetAssociatedProgram();
+            if(!File.Exists(associatedProgramPath))
                 return;
-            ProcessStartInfo info = new ProcessStartInfo(openSolutionPath, solutionFilePath);
+            ProcessStartInfo info = new ProcessStartInfo(associatedProgramPath, SolutionPath);
             info.Verb = "runas";
             Process.Start(info);
+        }
+        static string GetAssociatedProgram() {
+            try {
+                var objExtReg = Registry.ClassesRoot.OpenSubKey(".sln");
+                var strExtValue = objExtReg.GetValue("");
+                var objAppReg = Registry.ClassesRoot.OpenSubKey(strExtValue + @"\shell\open\command");
+                return (objAppReg.GetValue(null)?.ToString() ?? string.Empty).Replace("\"%1\"", String.Empty).Replace("%1", String.Empty).Replace("\"", string.Empty);
+            }
+            catch {
+                return @"C:\Program Files (x86)\Common Files\Microsoft Shared\MSEnv\VSLauncher.exe";
+            }
         }
         public void OpenFolder() {
             Process.Start(Path);
