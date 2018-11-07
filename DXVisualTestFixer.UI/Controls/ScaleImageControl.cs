06ba1de480312461c494e0a20d6b90c9000b2c16
zinovyev.petr <petr.zinovyev@devexpress.com>
2018-11-07 12:36:32 +0300

    add grid lines

--- DXVisualTestFixer.UI/Controls/ScaleImageControl.cs
+++ DXVisualTestFixer.UI/Controls/ScaleImageControl.cs
@@ -6,6 +6,7 @@ using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,35 +18,20 @@ using Size = System.Windows.Size;
 
 namespace DXVisualTestFixer.UI.Controls {
     public class ScaleImageControl : FrameworkElement {
-        #region Inner Classes
-
-        class MyColor {
-            public readonly byte A;
-            public readonly byte R;
-            public readonly byte G;
-            public readonly byte B;
-
-            public MyColor(byte a, byte r, byte g, byte b) {
-                A = a;
-                R = r;
-                G = g;
-                B = b;
-            }
-        }
-
-        #endregion
-
         public static readonly DependencyProperty ImageSourceProperty;
         public static readonly DependencyProperty ScaleProperty;
+        public static readonly DependencyProperty ShowGridLinesProperty;
 
         static ScaleImageControl() {
             Type ownerType = typeof(ScaleImageControl);
             ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(BitmapSource), ownerType,
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsRender,
-                (d, e) => ((ScaleImageControl)d).OnImageSourceChanged()));
+                (d, e) => ((ScaleImageControl)d).Refresh()));
             ScaleProperty = DependencyProperty.Register("Scale", typeof(int), ownerType,
                 new FrameworkPropertyMetadata(1, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsRender,
-                (d, e) => ((ScaleImageControl)d).OnImageSourceChanged()));
+                (d, e) => ((ScaleImageControl)d).Refresh()));
+            ShowGridLinesProperty = DependencyProperty.Register("ShowGridLines", typeof(bool), ownerType, new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsRender,
+                (d, e) => ((ScaleImageControl)d).Refresh()));
         }
 
         public BitmapSource ImageSource {
@@ -56,9 +42,13 @@ namespace DXVisualTestFixer.UI.Controls {
             get { return (int)GetValue(ScaleProperty); }
             set { SetValue(ScaleProperty, value); }
         }
+        public bool ShowGridLines {
+            get { return (bool)GetValue(ShowGridLinesProperty); }
+            set { SetValue(ShowGridLinesProperty, value); }
+        }
 
         ImageSource scaledImage = null;
-        void OnImageSourceChanged() {
+        void Refresh() {
             scaledImage = ScaleImage();
         }
 
@@ -66,15 +56,6 @@ namespace DXVisualTestFixer.UI.Controls {
             if(scaledImage == null)
                 return Size.Empty;
             return new Size(scaledImage.Width, scaledImage.Height);
-        }
-
-        static Bitmap ResizeImage(Bitmap image, int width, int height) {
-            Bitmap resizedImage = new Bitmap(width, height);
-            using(Graphics gfx = Graphics.FromImage(resizedImage)) {
-                gfx.DrawImage(image, new Rectangle(0, 0, width, height),
-                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
-            }
-            return resizedImage;
         }
 
         static Bitmap BitmapSourceToBitmap(BitmapSource srs) {
@@ -86,7 +67,14 @@ namespace DXVisualTestFixer.UI.Controls {
             }
         }
 
-        static Bitmap ResizeImage(Image image, int scale) {
+        static byte[] ImageToByteArray(Image imageIn) {
+            using(var ms = new MemoryStream()) {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        static Bitmap ResizeImage(Image image, int scale, bool showGridLines) {
             var destRect = new Rectangle(0, 0, image.Width * scale, image.Height * scale);
             Bitmap destImage = new Bitmap(destRect.Width, destRect.Height);
 
@@ -94,10 +82,24 @@ namespace DXVisualTestFixer.UI.Controls {
 
             using(var graphics = Graphics.FromImage(destImage)) {
                 graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
-
+                graphics.CompositingQuality = CompositingQuality.AssumeLinear;
+                graphics.SmoothingMode = SmoothingMode.None;
+                graphics.PixelOffsetMode = PixelOffsetMode.Half;
+                graphics.PageUnit = GraphicsUnit.Pixel;
                 using(var wrapMode = new ImageAttributes()) {
                     wrapMode.SetWrapMode(WrapMode.TileFlipXY);
                     graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    if(showGridLines && scale > 1) {
+                        float thickness = 1f;
+                        var pen = new System.Drawing.Pen(System.Drawing.Color.Gray, thickness);
+                        pen.Alignment = PenAlignment.Right;
+                        for(int x = scale; x < destRect.Width; x += scale) {
+                            graphics.DrawLine(pen, x, 0, x, destRect.Width);
+                        }
+                        for(int y = scale; y < destRect.Height; y += scale) {
+                            graphics.DrawLine(pen, 0, y, destRect.Height, y);
+                        }
+                    }
                 }
             }
 
@@ -110,11 +112,11 @@ namespace DXVisualTestFixer.UI.Controls {
         BitmapSource ScaleImage() {
             if(ImageSource == null)
                 return null;
-            return SafeScaleImage(Scale);
+            return SafeScaleImage();
         }
-        BitmapSource SafeScaleImage(int scale) {
+        BitmapSource SafeScaleImage() {
             try {
-                Bitmap result = ResizeImage(BitmapSourceToBitmap(ImageSource), scale);
+                Bitmap result = ResizeImage(BitmapSourceToBitmap(ImageSource), Scale, ShowGridLines);
                 return Convert(result);
             }
             catch {
@@ -128,6 +130,16 @@ namespace DXVisualTestFixer.UI.Controls {
             if(scaledImage == null)
                 return;
             drawingContext.DrawImage(scaledImage, new Rect(new Size(scaledImage.Width, scaledImage.Height)));
+            //if(Scale < 3)
+            //    return;
+            //double thickness = 1;
+            //var pen = new System.Windows.Media.Pen(new SolidColorBrush(Colors.Gray), thickness);
+            //for(int x = Scale - 1; x < scaledImage.Width; x += Scale) {
+            //    drawingContext.DrawLine(pen, new System.Windows.Point(x - thickness / 2, 0), new System.Windows.Point(x - thickness / 2, scaledImage.Height));
+            //}
+            //for(int y = Scale - 1; y < scaledImage.Height; y += Scale) {
+            //    drawingContext.DrawLine(pen, new System.Windows.Point(0, y - thickness / 2), new System.Windows.Point(scaledImage.Width, y - thickness / 2));
+            //}
         }
     }
 }
