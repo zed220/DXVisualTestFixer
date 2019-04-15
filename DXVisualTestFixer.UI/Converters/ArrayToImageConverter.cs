f900f7d7acec7f5e999d1a2cb4d6f67c0a6a9092
zinovyev.petr <petr.zinovyev@devexpress.com>
2019-04-16 00:25:44 +0300

    add image size

--- DXVisualTestFixer.UI/Converters/ArrayToImageConverter.cs
+++ DXVisualTestFixer.UI/Converters/ArrayToImageConverter.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace DXVisualTestFixer.UI.Converters {
-    public class ArrayToImageConverter : IValueConverter {
-        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+    public class ArrayToImageConverter : BaseValueConverter {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             byte[] imageData = value as byte[];
             if(imageData == null || imageData.Length == 0)
                 return null;
@@ -27,9 +29,24 @@ namespace DXVisualTestFixer.UI.Converters {
             image.Freeze();
             return image;
         }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotImplementedException();
+    }
+    public class ArrayToImageSizeConverter : ArrayToImageConverter {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var image = base.Convert(value, targetType, parameter, culture) as BitmapImage;
+            if(image == null)
+                return null;
+            return new Point(image.PixelWidth, image.PixelHeight);
+        }
+    }
+    public class EqualsPointsToForegroundConverter : BaseMultiValueConverter {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if(values.Length != 2)
+                return new SolidColorBrush(Colors.Black);
+            Point? first = values[0] as Point?;
+            Point? second = values[1] as Point?;
+            if(first == second)
+                return new SolidColorBrush(Colors.Green);
+            return new SolidColorBrush(Colors.Red);
         }
     }
 }
