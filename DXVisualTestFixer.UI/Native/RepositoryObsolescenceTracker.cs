6e45b7a3d381679acb7e5b160ab6bfe9d5a3f982
Petr Zinovyev <pzed220@gmail.com>
2019-09-30 11:29:16 +0300

    add repository obsolescence tracking

---
+++ DXVisualTestFixer.UI/Native/RepositoryObsolescenceTracker.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Windows.Threading;
+using DXVisualTestFixer.UI.ViewModels;
+
+namespace DXVisualTestFixer.UI.Native {
+	class RepositoryObsolescenceTracker {
+		readonly DispatcherTimer timer;
+		
+		public RepositoryObsolescenceTracker(Action onObsolescence) {
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromHours(1);
+			timer.Tick += (s, a) => {
+				timer.Stop();
+				onObsolescence();
+				timer.Start();
+			};
+		}
+
+		public void Stop() => timer.Stop();
+		public void Start() => timer.Start();
+	}
+}
