43480702164d54eedf54384f5289b0eb97788298
Petr Zinovyev <pzed220@gmail.com>
2019-10-17 16:03:03 +0300

    Refactoring

--- DXVisualTestFixer.UI/Converters/FilePathToImageSourceConverter.cs
+++ DXVisualTestFixer.UI/Converters/FilePathToImageSourceConverter.cs
@@ -5,15 +5,8 @@ using DXVisualTestFixer.UI.Models;
 
 namespace DXVisualTestFixer.UI.Converters {
 	public class FilePathToImageSourceConverter : IValueConverter {
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-			var repositoryFileModel = value as RepositoryFileModel;
-			if(repositoryFileModel == null)
-				return null;
-			return new Uri(repositoryFileModel.Path, UriKind.Absolute);
-		}
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => !(value is RepositoryFileModel repositoryFileModel) ? null : new Uri(repositoryFileModel.Path, UriKind.Absolute);
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-			throw new NotImplementedException();
-		}
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 	}
 }
