f70dcd918dbbff533cb86cd089490597cc59d217
Petr Zed <petr.zinovyev@devexpress.com>
2019-10-22 10:13:58 +0300

    Fix minio

--- DXVisualTestFixer.Core/Core/MinioWorker.cs
+++ DXVisualTestFixer.Core/Core/MinioWorker.cs
@@ -1,20 +1,41 @@
-﻿using System.IO;
-using System.Threading.Tasks;
-using DXVisualTestFixer.Common;
+﻿using DXVisualTestFixer.Common;
 using Minio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace DXVisualTestFixer.Core {
 	public class MinioWorker : IMinioWorker {
-		static readonly MinioClient minio = new MinioClient("gitlabci7-minio:9000", "xpfminio", "xpfminiostorage");
+		private const string bucketName = "visualtests";
+		
+		static readonly MinioClient minio = new MinioClient("gitlabci4-minio:9000", "xpfminio", "xpfminiostorage");
 
 		public async Task<string> Download(string path) {
 			try {
 				string result = null;
-				await minio.GetObjectAsync("visualtests", path, stream => {
+				await minio.GetObjectAsync(bucketName, path, stream => {
 					using var reader = new StreamReader(stream);
 					result = reader.ReadToEnd();
 				});
 				return result;
+			}
+			catch {
+				return null;
+			}
+		}
+		public async Task<string[]> Discover(string path) {
+			try {
+				var result = new List<string>();
+				var observable = minio.ListObjectsAsync(bucketName, path, false);
+				using var subscription = observable.Subscribe
+				(
+					item => result.Add(item.Key),
+					ex => throw ex
+				);
+				await observable.ToTask();
+				return result.ToArray();
 			}
 			catch {
 				return null;
---
+++ DXVisualTestFixer.Core/Core/Native/TestInfoCached.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using DXVisualTestFixer.Common;
+
+namespace DXVisualTestFixer.Core {
+	class TestInfoCached {
+		public TestInfoCached(Repository repository, string realUrl, List<TestInfo> testList, CorpDirTestInfoContainer container) {
+			Repository = repository;
+			RealUrl = realUrl;
+			TestList = testList;
+			UsedFilesLinks = container.UsedFilesLinks;
+			ElapsedTimes = container.ElapsedTimes;
+			Teams = container.Teams;
+			SourcesBuildTime = container.SourcesBuildTime;
+			TestsBuildTime = container.TestsBuildTime;
+		}
+
+		public Repository Repository { get; }
+		public string RealUrl { get; }
+		public List<TestInfo> TestList { get; }
+		public List<string> UsedFilesLinks { get; }
+		public List<IElapsedTimeInfo> ElapsedTimes { get; }
+		public List<Team> Teams { get; }
+		public DateTime? SourcesBuildTime { get; }
+		public DateTime? TestsBuildTime { get; }
+	}
+}
