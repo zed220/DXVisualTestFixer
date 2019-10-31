0fd8c20110db33ea2cf08a4cfb2c1a7791ee8d71
Petr Zed <petr.zinovyev@devexpress.com>
2019-10-31 13:23:52 +0300

    Add whats new

--- DXVisualTestFixer.UI/ViewModels/ShellViewModel.cs
+++ DXVisualTestFixer.UI/ViewModels/ShellViewModel.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 using DXVisualTestFixer.Common;
 using JetBrains.Annotations;
+using Microsoft.Practices.ServiceLocation;
 using Prism.Commands;
 using Prism.Interactivity.InteractionRequest;
 using Prism.Mvvm;
@@ -12,12 +18,16 @@ namespace DXVisualTestFixer.UI.ViewModels {
 	public class ShellViewModel : BindableBase {
 		readonly IDXNotification _notification;
 		readonly IUpdateService _updateService;
+		readonly DispatcherTimer _whatsNewTimer;
 
 		UpdateState _updateState = UpdateState.None;
 
 		public ShellViewModel(IUpdateService updateService, IDXNotification notification, INotificationService notificationService) {
 			_updateService = updateService;
 			_notification = notification;
+			_whatsNewTimer = new DispatcherTimer(DispatcherPriority.ContextIdle);
+			_whatsNewTimer.Tick += (sender, args) => ShowWhatsNewCore();
+			_whatsNewTimer.Interval = TimeSpan.FromSeconds(10);
 			notificationService.Notification += NotificationService_Notification;
 			Commands = new List<ICommand> {new DelegateCommand(Update).ObservesCanExecute(() => HasUpdate)};
 			if(updateService.HasUpdate) {
@@ -42,6 +52,39 @@ namespace DXVisualTestFixer.UI.ViewModels {
 				SetProperty(ref _updateState, value);
 				RaisePropertyChanged(nameof(HasUpdate));
 			}
+		}
+
+		[UsedImplicitly]
+		public void ShowWhatsNew() {
+			var configVersion = ServiceLocator.Current.GetInstance<IVersionService>().Version;
+			var configSerializer = ServiceLocator.Current.GetInstance<IConfigSerializer>();
+			var config = configSerializer.GetConfig();
+			var whatsNewVersion = new Version(config.WhatsNewSeenForVersion);
+			if(whatsNewVersion >= configVersion)
+				return;
+			_whatsNewTimer.Start();
+		}
+
+		void ShowWhatsNewCore() {
+			_whatsNewTimer.Stop();
+			
+			var configVersion = ServiceLocator.Current.GetInstance<IVersionService>().Version;
+			var configSerializer = ServiceLocator.Current.GetInstance<IConfigSerializer>();
+			var config = configSerializer.GetConfig();
+			var whatsNewVersion = new Version(config.WhatsNewSeenForVersion);
+			ViewModelBase.SetupNotification(_notification, "What's New", GetWhatsNewContent(configVersion, whatsNewVersion), MessageBoxImage.Information);
+			NotificationRequest.Raise(_notification);
+			config.WhatsNewSeenForVersion = configVersion.ToString();
+			configSerializer.SaveConfig(config);
+		}
+
+		string GetWhatsNewContent(Version configVersion, Version whatsNewVersion) {
+			var allInfos = ServiceLocator.Current.GetInstance<IVersionService>().WhatsNewInfo;
+			var sb = new StringBuilder();
+			foreach(var info in allInfos.Where(x => x.version > whatsNewVersion)) {
+				sb.AppendLine(info.content);
+			}
+			return sb.ToString();
 		}
 
 		bool HasUpdate => UpdateState == UpdateState.Ready;
--- DXVisualTestFixer/Services/VersionService.cs
+++ DXVisualTestFixer/Services/VersionService.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Documents;
+using System.Windows.Shapes;
+using DevExpress.Data;
 using DXVisualTestFixer.Common;
 using DXVisualTestFixer.Configuration;
 
 namespace DXVisualTestFixer.Services {
 	public class VersionService : IVersionService {
 		public Version Version => VersionInfo.Version;
+
+		public (Version version, string content)[] WhatsNewInfo { get; } = GetWhatsNewInfo(); 
+
+		static (Version version, string content)[] GetWhatsNewInfo() {
+			var result = new List<(Version version, string content)>();
+			using var stream = typeof(VersionService).Assembly.GetManifestResourceStream("DXVisualTestFixer.WhatsNew.txt");
+			using var sr = new StreamReader(stream);
+			while(!sr.EndOfStream) {
+				var line = sr.ReadLine();
+				var versionAndContent = line.Split(new [] { "::"}, StringSplitOptions.RemoveEmptyEntries);
+				if(versionAndContent.Length != 2)
+					continue;
+				result.Add((new Version(versionAndContent[0].Split(new [] { " "}, StringSplitOptions.RemoveEmptyEntries).Last()), versionAndContent[1]));
+			}
+			
+			return result.ToArray();
+		}
 	}
 }
