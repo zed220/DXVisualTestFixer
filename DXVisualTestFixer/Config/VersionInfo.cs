37233907a57b4982d9f5af6e7898eff0af53cec9
Petr Zed <petr.zinovyev@devexpress.com>
2019-11-11 10:19:09 +0300

    up version

--- DXVisualTestFixer/Config/VersionInfo.cs
+++ DXVisualTestFixer/Config/VersionInfo.cs
@@ -2,7 +2,7 @@
 
 namespace DXVisualTestFixer.Configuration {
 	public static class VersionInfo {
-		public const string VersionString = "3.3.7";
+		public const string VersionString = "3.3.9";
 		public static readonly Version Version = new Version(VersionString);
 	}
 }
