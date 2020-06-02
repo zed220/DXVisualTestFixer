ff0ade06d6e4f1092f62b0339b0caa8d6aca979c
Piter Zed <pzed220@gmail.com>
2020-06-02 23:39:41 +0300

    XfpDuty to XD and MessageBoxes on center screen

--- DXVisualTestFixer.UI/Common/InitialsExtractor.cs
+++ DXVisualTestFixer.UI/Common/InitialsExtractor.cs
@@ -4,6 +4,8 @@ using System.Linq;
 namespace DXVisualTestFixer.UI.Common {
 	static class InitialsExtractor {
 		public static string Extract(string fullName) {
+			if(fullName == "XpfDutyService")
+				return "XD";
 			var initials = fullName
 				.Replace(" ", string.Empty)
 				.Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries)
