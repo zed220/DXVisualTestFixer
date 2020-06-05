95c9a2511aa5ceda42c7ca33d8030d4541ea4fc4
Piter Zed <pzed220@gmail.com>
2020-06-05 12:32:13 +0300

    Add Dashboard

---
+++ DXVisualTestFixer.Core/Platforms/DashboardPlatformInfo.cs
@@ -0,0 +1,12 @@
+using DXVisualTestFixer.Common;
+
+namespace DXVisualTestFixer {
+	class DashboardPlatformInfo : IPlatformInfo {
+		public string Name => "Dashboard";
+		public string GitRepository => "http://gitserver/XPF/VisualTestsDashboard.git";
+		public string MinioRepository => "Dashboard";
+		public string LocalPath => "20{0}_VisualTests_Dashboard";
+		public string VersionsFileName => "versions_Dashboard.xml";
+		public string FarmTaskName => "Test.v{0} Dashboard VisualTests";
+	}
+}
