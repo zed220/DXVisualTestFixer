ae8868534f253fde35ae605fbb84e61eb2f039cc
Piter Zed <pzed220@gmail.com>
2020-06-06 00:53:15 +0300

    Move Dashboard to CrossPlatform

--- DXVisualTestFixer.Core/Platforms/DashboardPlatformInfo.cs
+++ DXVisualTestFixer.Core/Platforms/DashboardPlatformInfo.cs
@@ -3,7 +3,7 @@ using DXVisualTestFixer.Common;
 namespace DXVisualTestFixer {
 	class DashboardPlatformInfo : IPlatformInfo {
 		public string Name => "Dashboard";
-		public string GitRepository => "http://gitserver/XPF/VisualTestsDashboard.git";
+		public string GitRepository => "http://gitserver/CrossPlatform/VisualTestsDashboard.git";
 		public string MinioRepository => "Dashboard";
 		public string LocalPath => "20{0}_VisualTests_Dashboard";
 		public string VersionsFileName => "versions_Dashboard.xml";
