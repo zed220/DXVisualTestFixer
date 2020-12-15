e8aef23d8a9849eccfb7cb30e1bb569a1b0f5b26
Petr Zed <petr.zinovyev@devexpress.com>
2020-12-15 11:36:19 +0300

    Fix config path

--- DXVisualTestFixer.Common/RepositoryLoader.cs
+++ DXVisualTestFixer.Common/RepositoryLoader.cs
@@ -3,7 +3,7 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace DXVisualTestFixer.Common {
 	public class RepositoryLoader : FileStringLoaderBase {
-		RepositoryLoader(IPlatformInfo platform) : base(@"\\corp\internal\common\visualTests_squirrel\" + platform.VersionsFileName) { }
+		RepositoryLoader(IPlatformInfo platform) : base(@"\\corp\internal\common\visualTests_squirrel\settings\" + platform.VersionsFileName) { }
 
 		public static string[] GetVersions(IPlatformInfo platform) => new RepositoryLoader(platform).Result.ToArray();
 		protected override List<string> LoadIfFileNotFound() => new List<string> { "20.1", "20.2" };
